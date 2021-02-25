c2a8fa60bfffa4573c58a1ec09280a42b2200aae
Lokathor <zefria@gmail.com>
2021-02-25 07:22:25 +0000

    Deploying to gh-pages from  @ af6e1e55b5ee7eb94c4437788c4a6f09af8f1621 🚀

--- implementors/core/convert/trait.From.js
+++ implementors/core/convert/trait.From.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["vkvk"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;bool&gt; for <a class=\"struct\" href=\"vkvk/prelude/struct.VkBool32.html\" title=\"struct vkvk::prelude::VkBool32\">VkBool32</a>","synthetic":false,"types":["vkvk::vk_bool::VkBool32"]}];
+implementors["vkvk"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;bool&gt; for <a class=\"struct\" href=\"vkvk/enumerations/struct.VkBool32.html\" title=\"struct vkvk::enumerations::VkBool32\">VkBool32</a>","synthetic":false,"types":["vkvk::enumerations::vk_bool::VkBool32"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- source-files.js
+++ source-files.js
@@ -1,4 +1,4 @@
 var N = null;var sourcesIndex = {};
 sourcesIndex["chlorine"] = {"name":"","files":["lib.rs"]};
-sourcesIndex["vkvk"] = {"name":"","dirs":[{"name":"version_1_0","files":["enumerations.rs","flag_bits.rs","structures.rs"]}],"files":["handles.rs","lib.rs","macros.rs","non_dispatchable_handles.rs","vk_bool.rs","vk_platform.rs","vk_result.rs","vk_structure_type.rs","vk_version.rs"]};
+sourcesIndex["vkvk"] = {"name":"","dirs":[{"name":"enumerations","files":["vk_bool.rs","vk_result.rs","vk_structure_type.rs"]}],"files":["enumerations.rs","flag_bits.rs","handles.rs","lib.rs","macros.rs","non_dispatchable_handles.rs","structures.rs","vk_platform.rs","vk_version.rs"]};
 createSourceSidebar();
