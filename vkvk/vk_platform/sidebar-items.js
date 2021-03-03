e2056dc141db673928f32ca83107426c17dafae8
Lokathor <zefria@gmail.com>
2021-03-03 01:58:26 +0000

    Deploying to gh-pages from  @ bd0eda36c7c3681b8929cd5314a91bb6711d86ca 🚀

--- vkvk/vk_platform/sidebar-items.js
+++ vkvk/vk_platform/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["c_void","Equivalent to C’s `void` type when used as a pointer."]],"type":[["int","Basically this is `i32`"]]});
+initSidebarItems({"enum":[["c_void","Equivalent to C’s `void` type when used as a [pointer]."]],"type":[["int","Basically this is `i32`"]]});
