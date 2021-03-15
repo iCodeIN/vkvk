da7ba9040076c8eae2cb37f13f5e6c507aa3bcfe
Lokathor <zefria@gmail.com>
2021-03-15 22:21:32 +0000

    Deploying to gh-pages from  @ a4097a8f240abf7dc145ab580023a7f670c50560 🚀

--- source-files.js
+++ source-files.js
@@ -1,4 +1,4 @@
 var N = null;var sourcesIndex = {};
 sourcesIndex["chlorine"] = {"name":"","files":["lib.rs"]};
-sourcesIndex["vkvk"] = {"name":"","dirs":[{"name":"enumerations","files":["vk_bool.rs","vk_format.rs","vk_result.rs","vk_structure_type.rs"]}],"files":["enumerations.rs","flag_bits.rs","fn_managers.rs","fn_managers_rusty.rs","fn_types.rs","handles.rs","lib.rs","macros.rs","non_dispatchable_handles.rs","structures.rs","vk_platform.rs","vk_version.rs"]};
+sourcesIndex["vkvk"] = {"name":"","dirs":[{"name":"enumerations","files":["vk_bool.rs","vk_format.rs","vk_result.rs","vk_structure_type.rs"]}],"files":["enumerations.rs","flag_bits.rs","fn_managers.rs","fn_managers_rusty.rs","fn_types.rs","handles.rs","lib.rs","macros.rs","non_dispatchable_handles.rs","structures.rs","unions.rs","vk_platform.rs","vk_version.rs"]};
 createSourceSidebar();
---
+++ vkvk/unions/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"union":[["VkClearColorValue","VkClearColorValue"],["VkClearValue","VkClearValue"]]});
